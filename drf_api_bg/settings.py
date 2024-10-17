c497f4bfa02ec2565e4c11a9fd4244d0e532634c
JonathanDussot <jonathandussot@gmail.com>
2024-10-17 10:00:57 +0000

    Modified CORS_ALLOWED_ORIGIN to allow me to deploy to Heroku.

--- drf_api_bg/settings.py
+++ drf_api_bg/settings.py
@@ -112,7 +112,7 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN').rstrip('/')
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
