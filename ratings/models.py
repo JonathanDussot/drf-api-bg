08193e8587741ddfafa646035a4a64e6290c60ae
Jonathan Dussot <jonathandussot@gmail.com>
2024-10-21 11:13:49 +0000

    Modified ratings and reviews files to be pep8 compliant.

--- ratings/models.py
+++ ratings/models.py
@@ -3,18 +3,23 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from games.models import Game
 
+
 class Rating(models.Model):
-    """ 
+    """
     Rating model with related name ratings
     """
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(6)],
         default=3,
     )
-    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='ratings')
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name='ratings'
+    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-created_at']
         unique_together = ['owner', 'game']
--- ratings/serializers.py
+++ ratings/serializers.py
@@ -2,18 +2,24 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Rating
 
+
 class RatingSerializer(serializers.ModelSerializer):
     """
     Serializer for the ratings model.
     """
-    
+
     owner = serializers.ReadOnlyField(source='owner.username')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    
+
     class Meta:
         model = Rating
-        fields = ['id', 'owner', 'profile_image', 'rating', 'game', 'created_at']
-        
+        fields = [
+            'id',
+            'owner',
+            'profile_image',
+            'rating', 'game',
+            'created_at'
+            ]
 
     def validate(self, data):
         """
@@ -21,13 +27,15 @@ class RatingSerializer(serializers.ModelSerializer):
         """
         user = self.context['request'].user
         game = data['game']
-        
-        if self.instance is None and Rating.objects.filter(owner=user, game=game).exists():
-            raise serializers.ValidationError('You have already rated this game.')
-        
+
+        if (
+            self.instance is None
+            and Rating.objects.filter(owner=user, game=game).exists()
+        ):
+            raise serializers.ValidationError('You already rated this game.')
+
         return data
-        
-        
+
     def create(self, validated_data):
         """
         Validation to determine is possible duplicate of rating
