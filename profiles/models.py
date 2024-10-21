1d751fd5e265f07dae2cb5a2bbfaa1d93069f588
Jonathan Dussot <jonathandussot@gmail.com>
2024-10-21 11:04:50 +0000

    Modified likes and profiles files to be pep8 compliant.

--- profiles/models.py
+++ profiles/models.py
@@ -23,11 +23,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
+
 def create_profile(sender, instance, created, **kwargs):
     """
     Creates profile when new user is created
     """
     if created:
         Profile.objects.create(owner=instance)
+
 
 post_save.connect(create_profile, sender=User)
